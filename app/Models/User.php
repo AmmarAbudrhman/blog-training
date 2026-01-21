45a8641c689586080b0fb88dc8979de12f705480
ALI <a711879040@gmail.com>
2026-01-21 21:16:50 +0300

    Add comments relationship to User model for managing user comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,10 +65,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
-
-
-
     }
 
-
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
